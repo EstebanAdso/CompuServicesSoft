141fc4a2c6fb74298a2158c86a0cfe78d075d071
Esteban Trujillo <144358984+EstebanAdso@users.noreply.github.com>
2024-11-30 17:15:50 -0500

    Agregando carrurel de imagenes

--- js/app.js
+++ js/app.js
@@ -8,17 +8,12 @@ var imagenes = [
     'imagenes/setup.webp',
 ];
 
-// Índice de la imagen actual
 var indice = 1;
 
-// Función para cambiar la imagen cada 10 segundos
+
 function cambiarImagen() {
-    // Actualiza la URL de la imagen de fondo del div
     document.getElementById('containerPrincipal__img').style.backgroundImage = 'url(' + imagenes[indice] + ')';
-    
-    // Incrementa el índice para mostrar la próxima imagen
     indice = (indice + 1) % imagenes.length;
-   
 }
 
 setTimeout(() => {
@@ -36,43 +31,22 @@ function triggerAnimation() {
         link.classList.remove('link-grow-twice');
     }, 800); 
 }
-
 setInterval(triggerAnimation, 5000);
+triggerAnimation()
 
 
-    document.addEventListener('DOMContentLoaded', () => {
-            const carrusel = document.querySelector('.carrusel');
-            const productos = document.querySelectorAll('.producto');
-            const flechaIzquierda = document.querySelector('.flecha-izquierda');
-            const flechaDerecha = document.querySelector('.flecha-derecha');
-            let indiceActual = 2; // Producto 3 está inicialmente activo
+//CARRUSEL
+let next = document.querySelector('.button .next')
+let prev = document.querySelector('.button .prev')
 
-            function actualizarCarrusel() {
-                productos.forEach((producto, index) => {
-                    producto.classList.remove('activo', 'cercano');
-                    
-                    if (index === indiceActual) {
-                        producto.classList.add('activo');
-                    }
-                    
-                    if (index === indiceActual - 1 || index === indiceActual + 1) {
-                        producto.classList.add('cercano');
-                    }
-                });
-            }
+next.addEventListener('click',function(){
+    let items = document.querySelectorAll('.slide .item')
+    document.querySelector('.slide').appendChild(items[0])
+    
+})
 
-            flechaDerecha.addEventListener('click', () => {
-                if (indiceActual < productos.length - 1) {
-                    indiceActual++;
-                    actualizarCarrusel();
-                }
-            });
-
-            flechaIzquierda.addEventListener('click', () => {
-                if (indiceActual > 0) {
-                    indiceActual--;
-                    actualizarCarrusel();
-                }
-            });
-        });
-
+prev.addEventListener('click',function(){
+    let items = document.querySelectorAll('.slide .item')
+    document.querySelector('.slide').prepend(items[items.length - 1])
+    
+})
