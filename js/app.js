d2b5af8b2716f62fde772c5abe077e068f4b3d8d
Esteban} <estebanadso@gmail.com>
2024-12-12 11:31:49 -0500

    diseño compacto para celulares

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-var imagenes = [
+let imagenes = [
     'imagenes/fondos/pc-gaming-setup-rgb-4k-ft1ym37yjyb7lp19.jpg',
     'imagenes/fondos/set2.jpg',
     'imagenes/fondos/set3.jpg',
@@ -8,16 +8,42 @@ var imagenes = [
     'imagenes/fondos/setup.webp',
 ];
 
-var indice = 0; // Comenzamos desde el primer índice
+let indice = 0; // Comenzamos desde el primer índice
 
 function cambiarImagen() {
     document.getElementById('containerPrincipal__img').style.backgroundImage = 'url(' + imagenes[indice] + ')';
     indice = (indice + 1) % imagenes.length; 
 }
 
-
 setInterval(cambiarImagen, 10000);
 cambiarImagen();
+
+// let textos = [
+//     'Ven y cumple el sueño de todo Gamer!',
+//     'Tu tienda de tecnología  de confianza',
+//     'CompuServicesSoft:  Innovación para tu PC',
+//     'Soluciones en productos  y mantenimiento de computadoras',
+//     'Equipos y accesorios  para un rendimiento superior',
+//     'CompuServicesSoft:  Todo en tecnología a tu alcance',
+//     'Repara y mejora  tu PC con nosotros',
+//     'La mejor tecnología para tu equipo',
+//     'Asesoría, venta y reparación de computadoras',
+//     'Potencia tu PC con nuestros productos y servicios',
+//     'CompuServicesSoft: Expertos en tecnología y mantenimiento'
+// ];
+
+
+// let indiceTexto = 0
+
+// function cambiarTexto(){
+//     const parrafoPrincipal = document.querySelector('#parrafoPrincipal');
+//     parrafoPrincipal.innerHTML = textos[indiceTexto]
+//     indiceTexto = (indiceTexto + 1) % textos.length
+// }
+// setInterval(cambiarTexto, 10000)
+// cambiarTexto()
+
+
 
 //Parpadear Boton De bajar
 link = document.querySelector('.containerPrincipal__down'); 
